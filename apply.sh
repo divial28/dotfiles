f1f191f6b0915ae9e720887f0b5c7e72fac9b415
divial28 <dimvyalkov97@gmail.com>
2024-07-27 22:28:45 +0300

    little i3 tune + inital polybar config

--- apply.sh
+++ apply.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-cp ./.config/i3/config ~/.config/i3/config
+rsync -a ./.config/i3/config ~/.config/i3/
+rsync -a ./.config/polybar/launch.sh ~/.config/polybar/
+rsync -a ./.config/polybar/config.ini ~/.config/polybar/
