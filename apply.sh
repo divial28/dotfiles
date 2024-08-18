69288856e632f328fddbf40769b08761658affe0
divial28 <dimvyalkov97@gmail.com>
2024-08-18 15:29:27 +0300

    add rofi config

--- apply.sh
+++ apply.sh
@@ -2,6 +2,7 @@
 rsync -a ./.config/i3/config ~/.config/i3/
 rsync -a ./.config/polybar/launch.sh ~/.config/polybar/
 rsync -a ./.config/polybar/config.ini ~/.config/polybar/
+rsync -a ./.config/rofi ~/.config/
 
 # rsync -a ./polybar-scripts/polybar-scripts/system-bluetooth-bluetoothctl/system-bluetooth-bluetoothctl.sh ~/.config/polybar/scripts/
 # chmod +x ~/.config/polybar/scripts/system-bluetooth-bluetoothctl.sh
@@ -10,6 +11,5 @@ rsync -a ./polybar-bluetooth/bluetooth.sh ~/.config/polybar/scripts/
 rsync -a ./polybar-bluetooth/toggle_bluetooth.sh ~/.config/polybar/scripts/
 chmod +x ~/.config/polybar/scripts/bluetooth.sh
 chmod +x ~/.config/polybar/scripts/toggle_bluetooth.sh
-
 rsync -a ./polybar-pulseaudio-control/pulseaudio-control ~/.config/polybar/scripts/
 chmod +x ~/.config/polybar/scripts/pulseaudio-control
